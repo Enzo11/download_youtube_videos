93b6e0b4c0531e30531b91302ef956427930eca3
Av <ayaanvirani0@gmail.com>
2020-03-21 03:43:22 -0400

    Update download.py

--- download.py
+++ download.py
@@ -4,8 +4,9 @@ video = input("Pase the url here: ")
 
 video_url = video # paste here your Youube videos' url
 youtube = pytube.YouTube(video_url)
-video = youtube.streams.first()
-video.download('/home/king/Videos/ccna_youtube_turtorial') # path, where to video download.
+video = youtube.streams.get_highest_resolution().download('/home/king/Videos/mytool_download_youtube_video')
+#video.download('/home/king/Videos/mytool_download_youtube_video') # path, where to video download.
 
 
 
+#YouTube('http://youtube.com/watch?v=9bZkp7q19f0').streams[0].download()
